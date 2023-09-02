74409cc9b990c33f9c1f1bf9867ff90cc6fee36f
Lucas Cardoso <lucas_cardoso_de_castro@hotmail.com>
2023-09-01 21:38:36 -0300

    Adicionar arquivos de projeto.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Combustivel_MAUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace Combustivel_MAUI;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCalcClicked(object sender, EventArgs e)
+    {
+        double.TryParse(TextValorAlcool.Text, out double valorAlcool); //Pega numero do Entry e insere em "valor"
+        double.TryParse(TextValorGasolina.Text, out double valorGasolina);
+
+        if (valorAlcool / valorGasolina < 0.7)
+        {
+            TextResultado.Text = $"⛽ Abasteça com Álcool!";
+        }
+        else
+        {
+            TextResultado.Text = $"⛽ Abasteça com Gasolina!";
+        }
+    }
+}
+
